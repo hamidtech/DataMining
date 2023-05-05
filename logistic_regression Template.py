667a57e4e812f27b9571138c2e016e61453d3c71
Hamid Jamali <34779844+hamidtech@users.noreply.github.com>
2023-05-05 11:49:07 +0330

    Create Logistic tem.plate & Decission tree training

---
+++ logistic_regression Template.py
@@ -0,0 +1,108 @@
+# Logistic Regression
+
+# Importing the Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+# Defining Independent and Dependent Variables
+
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+# Which Regression?
+
+plt.scatter(X[:,0], y, color = 'red')
+plt.scatter(X[:,1], y, color = 'red')
+
+
+# Splitting the dataset into training set and test set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+
+SC_X = StandardScaler()
+X_train = SC_X.fit_transform(X_train)
+X_test = SC_X.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state= 0)
+classifier.fit(X_train, y_train)
+
+# Prediciting the Test set results and X_new
+
+y_pred = classifier.predict(X_test)
+
+X_new = SC_X.fit_transform(np.array([[19, 19000]], dtype = float)) # it should be normalized
+classifier.predict(X_new)
+
+# Making the Confusion Matrix
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# accuracy = (TP+TN)/(TP+FP+TN+FN)
+# recall = TP/(TP+FN)
+# precision = TP/(TP+FP)
+
+from sklearn.metrics import precision_recall_fscore_support
+precision_recall_fscore_support(y_test, y_pred)
+
+# Visualising the Training set results
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, \
+                               stop = X_set[:, 0].max() + 1, step = 0.01), \
+                     np.arange(start = X_set[:, 1].min() - 1, \
+                               stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T). \
+             reshape(X1.shape), alpha = 0.5, camp = ListedColormap(('red', 'green')))   
+ 
+plt.xlim(X1.min(), X1.max())    
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], \
+                X_set[y_set == j, 1], c = ListedColormap(('red', 'green')) \
+                    (i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, \
+                               stop = X_set[:, 0].max() + 1, step = 0.01), \
+                     np.arange(start = X_set[:, 1].min() - 1, \
+                               stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T). \
+             reshape(X1.shape), alpha = 0.5, camp = ListedColormap(('red', 'green')))   
+ 
+plt.xlim(X1.min(), X1.max())    
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], \
+                X_set[y_set == j, 1], c = ListedColormap(('red', 'green')) \
+                    (i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
